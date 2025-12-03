e9ea3fe2480dae525fbe5146f8eaf614cb298932
Xander UijtdeHaag <uijt0009@hz.nl>
2025-12-03 15:09:34 +0100

    Schatkaart pagina gemaakt en gestyled, achtergronduploader toegevoegd aan de kalenderpagina

--- src/routes/kalender/+page.svelte
+++ src/routes/kalender/+page.svelte
@@ -1,11 +1,16 @@
 <script>
   import CalendarGrid from '$lib/components/calendar/CalendarGrid.svelte';
   import NewTasksSection from '$lib/components/tasks/NewTasksSection.svelte';
+  import AchtergrondUploader from '$lib/components/schatkaart/AchtergrondUploader.svelte';
 </script>
 
-<div class="p-4 sm:p-8 bg-gray-50 min-h-screen">
-  
-  <CalendarGrid /> 
-  <NewTasksSection />
+<div class="p-4 sm:p-8 bg-gray-50 min-h-screen relative">
+    
+    <div class="flex justify-end mb-4 z-50 relative"> 
+        <AchtergrondUploader on:upload={() => { /* Stil succes! */ }} />
+    </div>
+
+    <CalendarGrid /> 
+    <NewTasksSection />
   
 </div>
