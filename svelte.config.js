b410a1d74ff61892db3d380a96328e9c28415895
R.G.A de Cuijper <cuij0003@hz.nl>
2025-12-03 12:49:59 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,6 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
   kit: {
-    adapter: adapter({
-      out: 'build',       // optional
-      precompress: false,
-      envPrefix: 'VITE_'
-    }),
-    vite: {
-      server: {
-        host: '0.0.0.0',  // <--- make server accessible outside container
-        port: 4173
-      }
-    }
+    adapter: adapter()
   }
 };
