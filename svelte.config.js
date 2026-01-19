1f05a430d7f2344520b84d374d9b36424a5f3071
R.G.A de Cuijper <cuij0003@hz.nl>
2026-01-19 19:43:08 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 export default {
+  preprocess: preprocess(),
   kit: {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: 'index.html'
-    })
+      fallback: null
+    }),
+    paths: {
+      base: dev ? '' : '/UVC-Project-Front-End'
+    }
   }
 };
-
