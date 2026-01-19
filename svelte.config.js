ad574faaaee31e37a23e51f2a74542beb7875927
R.G.A de Cuijper <cuij0003@hz.nl>
2026-01-19 20:10:22 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,17 +2,18 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
-const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 export default {
   preprocess: preprocess(),
+
   kit: {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: 'null'
+      fallback: 'index.html' 
     }),
-     paths: {
+
+    paths: {
       base: dev ? '' : '/UVC-Project-Front-End'
     }
   }
